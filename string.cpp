86a3fe1854602c80f9558879464c19e9d8f43291
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2023-04-02 10:03:18 +0000

    finish string.cpp

--- string.cpp
+++ string.cpp
@@ -1,132 +1,162 @@
 #include <iostream>
-
+#include <stdexcept>
 using namespace std;
 
-class string
+class chaine
 {
 private:
   int msize;
   char *mt;
 
 public:
-  string();
-  string(char *);
-  string(string &);
-  ~string();
+  chaine();
+  chaine(char *);
+  chaine(chaine &);
+  ~chaine();
   int strlen();
-  char *getPointer();
-  string &operator=(const string &);
-  string operator+(const string &);
+  char *getMt();
+  chaine &operator=(const chaine &);
+  chaine operator+(const chaine &);
+  bool operator==(const chaine &);
   char &operator[](int);
-  void strcpy(const char &);
+  void strcpy(const char *);
   void strcat(const char *);
 };
 
-string::string()
+chaine::chaine()
 {
   msize = 0;
-  mt = nullptr;
+  mt = new char[1];
+  mt[0] = '\0';
 }
-string::string(char *_string)
+chaine::chaine(char *_chaine)
 {
   msize = 0;
-  while (s[++msize;] != '\0')
+  while (_chaine[msize] != '\0')
   {
     ++msize;
   }
   mt = new char[msize + 1];
-  for (int i = 0; i < msize; i++)
+  for (int i = 0; i <= msize; i++)
   {
-    mt[i] = _string[i];
+    mt[i] = _chaine[i];
   }
-  mt[msize] = '\0';
 }
-string::string(string &_string)
+chaine::chaine(chaine &_chaine)
 {
-  msize = _string.msize;
-  mt = new char[msize + 1];
-  for (int i = 0; i < msize; ++i)
+  msize = _chaine.msize;
+  mt = new char[msize];
+  for (int i = 0; i <= msize; i++)
   {
-    mt[i] = _string->mt[i];
+    mt[i] = _chaine.mt[i];
   }
-  mt[msize] = '\0';
 }
-string::~string()
+chaine::~chaine()
 {
-  delete[] str;
+  delete[] mt;
 }
-int string::strlen()
+int chaine::strlen()
 {
   return msize;
 }
-char *string::getPointer()
+char *chaine::getMt()
 {
   return mt;
 }
-string &string::operator=(const string &_string)
+chaine &chaine::operator=(const chaine &_chaine)
 {
-  if (this != &_string)
+  if (this != &_chaine)
   {
-    msize = _string.msize;
-    mt = new char[msize + 1];
-    for (int i = 0; i < msize; ++i)
+    delete[] mt;
+    mt = new char[msize = _chaine.msize];
+    for (int i = 0; i <= msize; ++i)
     {
-      mt[i] = _string->mt[i];
+      mt[i] = _chaine.mt[i];
     }
-    mt[msize] = '\0';
   }
   return *this;
 }
-string string::operator+(const string &_string)
+
+chaine chaine::operator+(const chaine &_chaine)
 {
-  string result;
-  result.msize = msize + _string.msize;
-  result->mt = new char[result.msize + 1];
+  chaine result;
+  result.msize = msize;
+  while (_chaine.mt[result.msize - msize] != '\0')
+  {
+    ++result.msize;
+  }
+  result.mt = new char[result.msize];
   for (int i = 0; i < msize; i++)
   {
-    result->mt[i] = mt[i];
+    result.mt[i] = mt[i];
   }
-  for (int i = 0, j = msize; j < result.msize && i < _string.msize; i++, j++)
+  for (int i = msize; i <= result.msize; i++)
   {
-    result->mt[i] = _string->mt[i];
+    result.mt[i] = _chaine.mt[i - msize];
   }
-  result->mt[result.mzise] = '\0';
   return result;
 }
-char &string::operator[](inti)
+char &chaine::operator[](int i)
 {
+  if (i < 0 || i >= msize)
+  {
+    throw out_of_range("Index out of range");
+  }
   return mt[i];
 }
-void string::strcpy(const char &_string)
+void chaine::strcpy(const char *_chaine)
 {
   msize = 0;
-  while (s[++msize;] != '\0')
+  while (_chaine[msize] != '\0')
   {
     ++msize;
   }
-  mt = new char[msize + 1];
+  mt = new char[msize];
+  for (int i = 0; i <= msize; i++)
+  {
+    mt[i] = _chaine[i];
+  }
+}
+void chaine::strcat(const char *_chaine)
+{
+  chaine temp;
+  temp.msize = msize;
+  while (_chaine[temp.msize - msize] != '\0')
+  {
+    ++temp.msize;
+  }
+  temp.mt = new char[temp.msize];
   for (int i = 0; i < msize; i++)
   {
-    mt[i] = _string[i];
+    temp.mt[i] = mt[i];
   }
-  mt[msize] = '\0';
+  for (int i = msize; i <= temp.msize; i++)
+  {
+    temp.mt[i] = _chaine[i - msize];
+  }
+  delete[] mt;
+  msize = temp.msize;
+  mt = temp.mt;
 }
-void string::strcat(const char &_string)
+bool chaine::operator==(const chaine &_chaine)
 {
-  int new_size = size;
-  while (s[new_size - size] != '\0')
-    ++new_size;
-  char *new_str = new char[new_size + 1];
-  for (int i = 0; i < size; ++i)
+  if (msize != _chaine.msize)
   {
-    new_str[i] = str[i];
+    return false;
   }
-  for (int i = size; i < new_size; ++i)
+  for (int i = 0; i < msize; ++i)
   {
-    new_str[i] = s[i - size];
+    if (mt[i] != _chaine.mt[i])
+    {
+      return false;
+    }
   }
-  new_str[new_size] = '\0';
-  delete[] str;
-  str = new_str;
-  size = new_size;
+  return true;
+}
+
+int main()
+{
+  // test
+
+  return 0;
 }
